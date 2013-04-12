4c4a6ae523bc7bac4be0fd4dc104b055161ad260
Eric Wong <normalperson@yhbt.net>
2013-04-12 02:05:54 +0000

    inotify: thread-safe Inotify#take for rbx
    Rubinius provides a Rubinius.synchronize helper for locking
    objects which do not otherwise have locks.  We need to
    synchronize Inotify#take access to prevent the internal array
    from being clobbered.
    
    This avoids unnecessary locking overhead on MRI which maintains
    a GVL.

--- lib/sleepy_penguin.rb
+++ lib/sleepy_penguin.rb
@@ -6,3 +6,18 @@ module SleepyPenguin
 end
 require 'sleepy_penguin_ext'
 require 'sleepy_penguin/epoll'
+
+# :stopdoc:
+#
+# We need to serialize Inotify#take for Rubinius since that has no GVL
+# to protect the internal array
+if defined?(SleepyPenguin::Inotify) &&
+   defined?(Rubinius) && Rubinius.respond_to?(:synchronize)
+  class SleepyPenguin::Inotify
+    alias __take take
+    undef_method :take
+    def take(*args)
+      Rubinius.synchronize(@inotify_tmp) { __take(*args) }
+    end
+  end
+end
